f9e0f6fea64c7bf203d5fd7f53ac2bd2eb008e09
saincogt <saincogt@gmail.com>
2020-05-29 14:50:30 +0930

    express and basic route setup

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
+
+// Connect DB
+connectDB();
 
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('API Running');
 });
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
