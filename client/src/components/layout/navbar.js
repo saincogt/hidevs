d89f4879acc3219be218cf519515174dd053de33
saincogt <saincogt@gmail.com>
2020-06-02 14:30:10 +0930

    minor fix to navbar

--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => (
 	<nav className='navbar bg-dark'>
 		<h1>
-			<Link to='index.html'><i className='fas fa-code'></i>DevConnector</Link>
+			<Link to='/'><i className='fas fa-code'></i>DevConnector</Link>
 		</h1>
 		<ul>
 			<li><Link to='!#'>Developers</Link></li>
