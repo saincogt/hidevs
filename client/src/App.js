ec59938b94d71375496fad74fabaed83bedad8a2
saincogt <saincogt@gmail.com>
2020-06-02 22:17:20 +0930

    implement create profile

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Landing from './components/layout/landing';
 import NavBar from './components/layout/navbar';
 import Alert from './components/layout/alert';
 import Dashboard from './components/dashboard/dashboard';
+import CreateProfile from './components/profile-form/create.profile';
 import Login from './components/auth/login';
 import Register from './components/auth/register';
 import PrivateRouter from './components/routing/privateroute';
@@ -15,32 +16,45 @@ import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+	setAuthToken(localStorage.token);
 }
 
 const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
 
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <NavBar />
-          <Route exact path='/' component={ Landing } />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={ Register } />
-              <Route exact path='/login' component={ Login } />
-              <PrivateRouter exact path='/dashboard' component={ Dashboard } />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
-}
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<NavBar />
+					<Route exact path='/' component={Landing} />
+					<section className='container'>
+						<Alert />
+						<Switch>
+							<Route
+								exact
+								path='/register'
+								component={Register}
+							/>
+							<Route exact path='/login' component={Login} />
+							<PrivateRouter
+								exact
+								path='/dashboard'
+								component={Dashboard}
+							/>
+							<PrivateRouter
+								exact
+								path='/create-profile'
+								component={CreateProfile}
+							/>
+						</Switch>
+					</section>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
 
 export default App;
