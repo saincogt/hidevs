277fa21f233b970e2dd71b1a8b9a2d80aeda77d7
saincogt <saincogt@gmail.com>
2020-06-03 21:30:08 +0930

    add profile experience and education

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/spinner';
 import ProfileTop from './profile.top';
 import ProfileAbout from './profile.about';
+import ProfileExperience from './profile.experience';
+import ProfileEducation from './profile.education';
 import { Link } from 'react-router-dom';
 import { getProfileById } from '../../actions/profile';
 
@@ -30,6 +32,36 @@ const Profile = ({ getProfileById, match, profile: { profile }, auth }) => {
 					<div class='profile-grid my-1'>
 						<ProfileTop profile={profile} />
 						<ProfileAbout profile={profile} />
+						<div className='profile-exp bg-white p-2'>
+							<h2 className='text-primary'>Experience</h2>
+							{profile.experience.length > 0 ? (
+								<Fragment>
+									{profile.experience.map(experience => (
+										<ProfileExperience
+											key={experience._id}
+											experience={experience}
+										/>
+									))}
+								</Fragment>
+							) : (
+								<h4>No experience credentials</h4>
+							)}
+						</div>
+						<div className='profile-edu bg-white p-2'>
+							<h2 className='text-primary'>Education</h2>
+							{profile.education.length > 0 ? (
+								<Fragment>
+									{profile.education.map(education => (
+										<ProfileEducation
+											key={education._id}
+											education={education}
+										/>
+									))}
+								</Fragment>
+							) : (
+								<h4>No education credentials</h4>
+							)}
+						</div>
 					</div>
 				</Fragment>
 			)}
