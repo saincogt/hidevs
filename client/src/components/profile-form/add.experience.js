74e088e6e717206c92060750eab90ea1c4bf999c
saincogt <saincogt@gmail.com>
2020-06-07 22:07:51 +0930

    remove unused framents

--- client/src/components/profile-form/add.experience.js
+++ client/src/components/profile-form/add.experience.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
