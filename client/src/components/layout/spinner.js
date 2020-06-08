71025858d16ac7f57390c21f1a5561ee8f1048a5
saincogt <saincogt@gmail.com>
2020-06-08 12:54:14 +0930

    add custom spinner

--- client/src/components/layout/spinner.js
+++ client/src/components/layout/spinner.js
@@ -1,14 +1,11 @@
-import React, { Fragment } from 'react';
-import spinner from './spinner.gif';
+import React from 'react';
+
+import { SpinnerContainer, SpinnerOverlay } from './spinner.styles';
 
 const Spinner = () => (
-	<Fragment>
-		<img
-			src={spinner}
-			style={{width: '200px', margin: 'auto', display: 'block' }}
-			alt='Loading...'
-		/>
-	</Fragment>
+	<SpinnerOverlay>
+		<SpinnerContainer />
+	</SpinnerOverlay>
 );
 
 export default Spinner;
